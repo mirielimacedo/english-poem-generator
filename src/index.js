67c7e1f330aa48445f404ae7ff1fba8c32803dac
mirielimacedo <mirielimacedo@gmail.com>
2024-01-18 13:22:15 +0800

    Updated javascript with API and CSS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 
-
-function generatorPoem(event){
-event.preventDefault();
-
-
-  new Typewriter("#poem", {
-    strings: "Golden Slumbers",
-    autoStart: true,
-    delay: 100,
-    cursor: "",
-  });
+function displayPoem(response) {
+    let poemElement = document.querySelector("#poem");
+    new Typewriter(poemElement, {
+        strings: response.data.answer, 
+        autoStart: true,
+        delay: 100, 
+        cursor: ""
+    }).start(); 
 }
 
+function generatorPoem(event) {
+    event.preventDefault();
 
- 
-let poemFormElement =  document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener('submit',generatorPoem);
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = 'f9b4d69b4d22303f3t650a59od86ede0'; 
+    let prompt = `generate the best English poem about element of nature ${instructionsInput.value}`; 
+    let context = "User instructions: you are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in html.No title. Sign in the poem with 'SheCode AI'and it shoud write in bold style" ;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
+    axios.get(apiUrl).then(displayPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener('submit', generatorPoem);
+
+
