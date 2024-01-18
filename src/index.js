fcbfeca5b6aa1dee14933997ce40abb4751e19f1
mirielimacedo <mirielimacedo@gmail.com>
2024-01-18 14:54:36 +0800

    improve the user experience

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ function generatorPoem(event) {
     let context = "User instructions: you are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in html.No title. Sign in the poem with 'SheCode AI'and it shoud write in bold style" ;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`; 
     axios.get(apiUrl).then(displayPoem);
+
+    let poemElement=document.querySelector("#poem");
+    poemElement.classList.remove("poem-hidden");
+    poemElement.innerHTML =`Generating a English poem about ${instructionsInput.value}`;
+
+   
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
